256c120b12e78c02cbd37e512c7978c0eb2eaf23
Jia Ying <jggoh@f0189887622b.ant.amazon.com>
2020-05-24 13:27:04 +0800

    add instructions

--- 01-TerraformDockerHelloWorld/main.tf
+++ 01-TerraformDockerHelloWorld/main.tf
@@ -4,17 +4,21 @@
 A provider is responsible for understanding API interactions and exposing resources.
 You can find a list of Terraform providers here: https://www.terraform.io/docs/providers/index.html
 Common provider includes docker and aws.
+
+This is indicating to use docker provider.
 */
-provider "docker" {}
+provider "<ADD_PROVIDER_HERE>" {}  #TODO: Indicate "docker" provider here
 
 
 /*
 This is a resource block. Each resource block describes one or more infrastructure objects.
+This block is equivalent to docker pull hello-world but now it is managed by Terraform.
+
 The resource block format is as such:
     resource "type" "local_name"{body block}
 
 The body block between { and } are the configuration arguments
 */
-resource "docker_image" "helloworld-image"{ 
-    name = "hello-world"
+resource "<ADD_RESOUCE_TYPE_HERE>"  "helloworld-image"{     #TODO: Define a resource type of "docker_image"
+    name = "<ADD_IMAGE_NAME HERE>"                          #TODO: Provide the image name "hello-world" to pull from docker registry
 }
--- 02-TerrafromDockerHttpd/main.tf
+++ 02-TerrafromDockerHttpd/main.tf
@@ -1,13 +1,15 @@
-resource "docker_image" "myapache-image" {
+# Define the provider required
+provider "docker" {}
+
+resource "docker_image" "<FILL_IN_TF_RESOURCE_NAME_HERE>" {    #TODO: Define a terraform resource name 
   name = "httpd"
 }
 
 resource "docker_container" "myapache-container" {
     name = "myapache"
-    image = docker_image.myapache-image.latest
+    image = docker_image.<REPLACE_WITH IMAGE_TF_RESOURCE_NAME>.latest   #TODO: Reference docker image that you have defined above her
     ports {
         internal = 80
         external = 8080
     }
 }
-
