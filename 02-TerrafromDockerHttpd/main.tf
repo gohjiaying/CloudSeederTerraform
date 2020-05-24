6394b87515551cdbcd4fc58ae0b7afa1ed52fbd8
Jia Ying <jggoh@f0189887622b.ant.amazon.com>
2020-05-24 15:56:10 +0800

    change container name

--- 02-TerrafromDockerHttpd/main.tf
+++ 02-TerrafromDockerHttpd/main.tf
@@ -6,7 +6,7 @@ resource "docker_image" "<FILL_IN_TF_RESOURCE_NAME_HERE>" {    #TODO: Define a t
 }
 
 resource "docker_container" "myapache-container" {
-    name = "myapache"
+    name = "mycontainer"
     image = docker_image.<REPLACE_WITH IMAGE_TF_RESOURCE_NAME>.latest   #TODO: Reference docker image that you have defined above her
     ports {
         internal = 80
