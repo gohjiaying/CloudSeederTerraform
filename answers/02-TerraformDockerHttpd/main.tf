1d200ae9340557668a9f0be27ce064ea3ec9e804
Jia Ying <jggoh@f0189887622b.ant.amazon.com>
2020-05-24 16:47:38 +0800

    update answer

--- answers/02-TerraformDockerHttpd/main.tf
+++ answers/02-TerraformDockerHttpd/main.tf
@@ -6,7 +6,7 @@ resource "docker_image" "myapache-image" {
 
 resource "docker_container" "myapache-container" {
     name = "mycontainer"
-    image = 
+    image = docker_image.myapache-image.latest
     ports {
         internal = 80
         external = 8080
